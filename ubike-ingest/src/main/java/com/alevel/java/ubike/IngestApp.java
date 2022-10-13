561333f2d6c8616c0383ea55b844cd88295fc34d
Alexander Tuleninov <talextu@gmail.com>
2022-10-13 19:16:36 +0300

    second commit - Waypoint

--- ubike-ingest/src/main/java/com/alevel/java/ubike/IngestApp.java
+++ ubike-ingest/src/main/java/com/alevel/java/ubike/IngestApp.java
@@ -14,6 +14,7 @@ public class IngestApp {
     private static final Logger log = LoggerFactory.getLogger(IngestApp.class);
 
     public static void main(String[] args) {
+        new IngestApp().runAddData();
         new IngestApp().run();
     }
 
